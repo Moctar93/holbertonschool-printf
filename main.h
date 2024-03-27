e64ae0691090bd2a462f72b772e438cc3a2f023c
root <moctartize93@gmail.com>
2024-03-27 13:45:47 +0000

    new files updates

--- main.h
+++ main.h
@@ -5,6 +5,19 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/**
+ * struct print_type - struct for printer functions
+ * @type_arg: format organizer
+ *
+ * @f: function to execute print
+ */
+
+typedef struct print_type
+{
+	char *type_arg;
+	int (*f)(va_list args);
+} print_t;
+
 int print_37(void);
 int print_d(va_list args);
 int print_i(va_list args);
